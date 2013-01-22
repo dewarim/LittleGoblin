bfb54af0a96e9ad07f971e43cf99bc790fc2ffe2
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-01-22 23:12:32 +0100

    Change source code level to Java 1.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,8 +3,8 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
-grails.project.target.level = 1.6
-grails.project.source.level = 1.6
+grails.project.target.level = 1.7
+grails.project.source.level = 1.7
 grails.project.war.file = "target/${appName}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
