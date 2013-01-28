c883b3dd1d81049e1a777e3f475945bc0ae351ee
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-01-28 22:30:36 +0100

    Add auto-generated test script.

---
+++ test/unit/de/dewarim/goblin/MyMailServiceTests.groovy
@@ -0,0 +1,16 @@
+package de.dewarim.goblin
+
+
+
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(MyMailService)
+class MyMailServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
