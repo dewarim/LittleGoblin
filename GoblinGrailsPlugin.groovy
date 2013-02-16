c8621a4ee3e800bc279c59e8a3faa9f068f44660
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-02-16 11:04:30 +0100

    More cleanup.

--- GoblinGrailsPlugin.groovy
+++ GoblinGrailsPlugin.groovy
@@ -7,47 +7,45 @@ class GoblinGrailsPlugin {
             "grails-app/views/error.gsp"
     ]
 
-    def title = "Little Goblin Plugin" 
+    def title = "Little Goblin Plugin"
     def author = "Ingo Wiarda"
     def authorEmail = "ingo_wiarda@dewarim.de"
-    def description = '''\
-Little Goblin is a browser game framework written in Grails.
-'''
+    def description = "Little Goblin is a browser game framework written in Grails."
 
     def documentation = "http://littlegoblin.de"
     def license = "APACHE"
     def developers = [
-            [ name: "Ingo Wiarda", email: "ingo_wiarda@dewarim.de" ]
+            [name: "Ingo Wiarda", email: "ingo_wiarda@dewarim.de"]
     ]
 
-    def issueManagement = [ system: "Github", url: "https://github.com/dewarim/LittleGoblin/issues" ]
-    def scm = [ url: "https://github.com/dewarim/LittleGoblin" ]
+    def issueManagement = [system: "Github", url: "https://github.com/dewarim/LittleGoblin/issues"]
+    def scm = [url: "https://github.com/dewarim/LittleGoblin"]
 
     def doWithWebDescriptor = { xml ->
-        
+
     }
 
     def doWithSpring = {
-        
+
     }
 
     def doWithDynamicMethods = { ctx ->
-        
+
     }
 
     def doWithApplicationContext = { applicationContext ->
-    
+
     }
 
     def onChange = { event ->
-        
+
     }
 
     def onConfigChange = { event ->
-        
+
     }
 
     def onShutdown = { event ->
-        
+
     }
 }
