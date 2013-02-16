94a392610f06e303fdb173f277982a7ed99d6d3f
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-02-16 10:58:31 +0100

    Moved dependencies from libs to BuildConfig. Fixes #21

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,6 +39,8 @@ grails.project.dependency.resolution = {
 
         // runtime 'mysql:mysql-connector-java:5.1.5'
         runtime 'postgresql:postgresql:9.1-901.jdbc4'
+        compile 'javax.mail:mail:1.4.5'
+        compile 'javax.activation:activation:1.1.1'
         compile("org.codehaus.groovy.modules.http-builder:http-builder:0.5.2"){
             excludes "groovy"
         }
