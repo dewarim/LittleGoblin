a36e9ff167882ce8c2aedce07c8b952def2aee49
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-02-16 11:03:13 +0100

    Cleanup plugin descriptor. Fixes #19

--- GoblinGrailsPlugin.groovy
+++ GoblinGrailsPlugin.groovy
@@ -1,73 +1,53 @@
 class GoblinGrailsPlugin {
 
-//    def packaging = "binary"
-    def groupId = 'goblin'
-    // the plugin version
     def version = "0.3.2"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
 
-    def title = "Little Goblin Plugin" // Headline display name of the plugin
+    def title = "Little Goblin Plugin" 
     def author = "Ingo Wiarda"
     def authorEmail = "ingo_wiarda@dewarim.de"
     def description = '''\
 Little Goblin is a browser game framework written in Grails.
 '''
 
-    // URL to the plugin's documentation
     def documentation = "http://littlegoblin.de"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
+    def developers = [
+            [ name: "Ingo Wiarda", email: "ingo_wiarda@dewarim.de" ]
+    ]
 
-    // Details of company behind the plugin (if there is one)
-    // def organization = [ name: "", url: "" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
     def issueManagement = [ system: "Github", url: "https://github.com/dewarim/LittleGoblin/issues" ]
-
-    // Online location of the plugin's browseable source code.
     def scm = [ url: "https://github.com/dewarim/LittleGoblin" ]
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
+        
     }
 
     def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
+        
     }
 
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
+        
     }
 
     def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
+    
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
+        
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
+        
     }
 
     def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
+        
     }
 }
