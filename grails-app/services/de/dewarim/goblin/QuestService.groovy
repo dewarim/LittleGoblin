0bdaa2a16c6793b7d14eb939c38eef5abcfcbb73
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-06 22:32:38 +0100

    Renamed field clazz to script.

--- grails-app/services/de/dewarim/goblin/QuestService.groovy
+++ grails-app/services/de/dewarim/goblin/QuestService.groovy
@@ -20,7 +20,7 @@ class QuestService {
     void executeScript(Encounter encounter,PlayerCharacter pc){
         IEncounterScript encounterScript
 		try{
-			encounterScript = encounter.script.clazz.newInstance()
+			encounterScript = encounter.script.script.newInstance()
 		}
         catch (ClassNotFoundException e){
             log.warn("Problem during executeScript: '${encounter.script.name}'",e)
