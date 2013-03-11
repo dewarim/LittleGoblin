a925353a216742c4e7c852f71ddd43d8bcdc8c56
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-11 22:44:22 +0100

    Add weapons to fixtures.

--- fixtures/game/dice.groovy
+++ fixtures/game/dice.groovy
@@ -8,6 +8,7 @@ fixture{
     d2x6p2(Dice, name:'2d6+2', amount:2, sides:6, bonus:2)
     d2x6(Dice, name:'2d6', amount:2, sides: 6)
     d2x5(Dice, name:'2d5', amount:2, sides: 5)
+    d2x5p2(Dice, name:'2d5+2', amount:2, sides: 5)
     d1x1p6(Dice, name:'1d1+6', amount: 1, sides: 1)
     d4(Dice, name:'1d4', sides: 4)
     initiative(Dice, name:'initiative', sides:20)
--- fixtures/game/items.groovy
+++ fixtures/game/items.groovy
@@ -1,5 +1,8 @@
+import de.dewarim.goblin.Dice
 import de.dewarim.goblin.EquipmentSlotType
 import de.dewarim.goblin.RequiredSlot
+import de.dewarim.goblin.combat.CombatAttributeType
+import de.dewarim.goblin.combat.WeaponAttribute
 import de.dewarim.goblin.item.ItemCategory
 import de.dewarim.goblin.item.ItemType
 import de.dewarim.goblin.item.ItemTypeFeature
@@ -10,6 +13,17 @@ include('game/features')
 def findSlotType(String name){
     return EquipmentSlotType.findByName(name)
 }
+
+Dice findDie(String name){
+    return Dice.findByName(name)
+}
+
+CombatAttributeType findCat(String name){
+    return CombatAttributeType.findByName(name)
+}
+
+def hand = findSlotType('slot.hand')
+def normalDam =  findCat('attribute.normal')
 
 fixture {
     healingPotion(ItemType, name: 'item.healing_potion',
@@ -33,7 +47,7 @@ fixture {
     swordIc(ItemCategory, itemType: sword, category: weapon)
     shield(ItemType, name: 'item.armor.shield', availability: 140, baseValue: 20)
     shieldIc(ItemCategory, itemType: shield, category: armor)
-    shieldSlot(RequiredSlot, itemType: shield, slotType: findSlotType('slot.hand'))
+    shieldSlot(RequiredSlot, itemType: shield, slotType: hand)
     
     clothArmor(ItemType, name:'armor.cloth', baseValue: 4)
     clothSlot(RequiredSlot, itemType:clothArmor, slotType: findSlotType('slot.body'))
@@ -41,5 +55,41 @@ fixture {
     leatherCap(ItemType, name: 'armor.leather_cap', baseValue: 4)
     capSlot(RequiredSlot, itemType: leatherCap, slotType:findSlotType('slot.head'))
     
+    fireWhip(ItemType,name: 'weapon.fire_whip', availability: 1000,
+            baseValue: 50, combatDice: findDie('2d6+2') )
+    whipAttr(WeaponAttribute, itemType:fireWhip, damageModifier:2.0, combatAttributeType:findCat('attribute.fire'))
+    whipSlot(RequiredSlot, itemType: fireWhip, slotType: hand)
     
+    shortSword(ItemType, name: 'weapon.short_sword', availability: 750,
+            baseValue: 5, combatDice: findDie('d6'))
+    swordAttr(WeaponAttribute, itemType: shortSword,damageModifier: 1.0, combatAttributeType: normalDam)
+    swordSlot(RequiredSlot, itemType: shortSword, slotType: hand) 
+    
+    longSword(ItemType, name: 'weapon.short_sword', availability: 750,
+            baseValue: 30, combatDice: findDie('2d6'))
+    longSwordAttr(WeaponAttribute, itemType:longSword, damageModifier: 1.0, combatAttributeType: normalDam)
+    longSwordSlot1(RequiredSlot, itemType: longSword, slotType: hand) // Long sword requires 2 slots. 
+    longSwordSlot2(RequiredSlot, itemType: longSword, slotType: hand)
+    
+    staff(ItemType, name: 'weapon.long_staff', availability: 350,
+            baseValue: 23, combatDice: findDie('2d5+2'))
+    staffAttr(WeaponAttribute, itemType:staff, damageModifier: 1.0, combatAttributeType: normalDam)
+    staffSlot1(RequiredSlot, itemType: staff, slotType: hand)
+    staffSlot2(RequiredSlot, itemType: staff, slotType: hand)
+    
+    pick(ItemType, name: 'weapon.toothpick_of_death', availability: 500,
+            baseValue: 10, combatDice: findDie('d6'))
+    pickAttr(WeaponAttribute, itemType:pick, damageModifier: 2, combatAttributeType: findCat('attribute.death'))
+    pickSlot(RequiredSlot, itemType: pick, slotType: hand)
+    
+    torch(ItemType, name: 'weapon.torch', availability: 1000,
+            baseValue: 1, combatDice: findDie('1d4'))
+    torchAttr(WeaponAttribute, itemType:torch, damageModifier: 1.25, combatAttributeType: findCat('attribute.fire'))
+    torchSlot(RequiredSlot, itemType: torch, slotType: hand)
+    
+    [fireWhip, shortSword, longSword, staff, pick, torch ].each{w ->
+        "${w.name.replaceAll('\\.','_')}"(ItemCategory, itemType:w, category:weapon)
+    }
+    whipIc(ItemCategory, itemType:fireWhip, category: magic)
+    pickIc(ItemCategory, itemType:pick, category: magic)
 }
