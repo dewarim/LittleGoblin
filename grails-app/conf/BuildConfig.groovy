053ea0de84e9972b339b8e4af88a5a9d15e56fd8
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-11 22:45:33 +0100

    Fixed BuildConfig
    (related to corrupted IDE files?)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,7 @@
 grails.servlet.version = "3.0"
-grails.project.work.dir = "target"
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.7
 grails.project.source.level = 1.7
 grails.project.war.file = "target/${appName}.war"
