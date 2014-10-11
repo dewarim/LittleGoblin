fd3a9e02edb3af4af85221806c0ba5cafe2a720f
Ingo Wiarda <ingo_wiarda@dewarim.de>
2014-10-11 11:43:22 +0200

    Add debug output to setTextOfElement.
    Check return value of getElementById and in case of missing id log a message to console.

--- grails-app/assets/javascripts/goblin.js
+++ grails-app/assets/javascripts/goblin.js
@@ -4,5 +4,11 @@ function hideRow(id){
 }
 
 function setTextOfElement(id, newText){
-    document.getElementById(id).innerHTML = newText;
+    var element = document.getElementById(id);
+    if(!element){
+        console.log("Failed to set text '"+newText+"' on missing element: #"+id);
+        return;
+    }
+    //console.log("set text of #"+id+" to "+newText);
+    element.innerHTML = newText;
 }
