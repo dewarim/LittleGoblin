b23893cd4832b3e63fcb6eddf629579b98045e80
Ingo Wiarda <ingo_wiarda@dewarim.de>
2014-10-27 23:15:46 +0100

    Add sub type for CombatAttributeType to differentiate between damage and resistance attributes.
    Related to #25

--- grails-app/domain/de/dewarim/goblin/combat/CombatAttributeType.groovy
+++ grails-app/domain/de/dewarim/goblin/combat/CombatAttributeType.groovy
@@ -1,5 +1,6 @@
 package de.dewarim.goblin.combat
 
+import de.dewarim.goblin.AttributeType
 import de.dewarim.goblin.CreatureAttribute
 
 /**
@@ -9,19 +10,21 @@ import de.dewarim.goblin.CreatureAttribute
 class CombatAttributeType {
 
     String name
-
+    AttributeType subType = AttributeType.ATTACK
+    
     static hasMany = [weaponAttributes:WeaponAttribute, creatureAttributes:CreatureAttribute]
     static constraints = {
         name(blank:false, unique:true)
     }
 
     boolean equals(o) {
-        if (is(o)) return true
+        if (this.is(o)) return true
         if (!(o instanceof CombatAttributeType)) return false
 
-        CombatAttributeType that = o
+        CombatAttributeType that = (CombatAttributeType) o
 
         if (name != that.name) return false
+        if (subType != that.subType) return false
 
         return true
     }
---
+++ src/java/de/dewarim/goblin/AttributeType.java
@@ -0,0 +1,16 @@
+package de.dewarim.goblin;
+
+/**
+ */
+public enum AttributeType {
+
+    /**
+     * A CombatAttributeType with subtype ATTACK is modifying the damage roll. 
+     */
+    ATTACK,
+
+    /**
+     * A CombatAttributeType with subtype RESISTANCE is modifying the damage resistance.
+     */
+    RESISTANCE
+}
