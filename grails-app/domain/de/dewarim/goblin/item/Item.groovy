e4e873afc216eaba550381b0f6f3b76c0cd3a037
Ingo Wiarda <ingo_wiarda@dewarim.de>
2014-11-02 09:17:58 +0100

    Changed field owner from Creature to PlayerCharacter.
    Reason: I was having a hard time using creating a unit test that would work with the extending class PlayerCharacter with this field.
    Then I changed Creature to be an abstract class as there won't be any Creature objects instantiated directly anyway.
    Now it looks like the ProductionServiceSpec will run without problems.
    Only drawback: Mobs which extends Creature cannot own Items now. But then was already a need to write new code for Mobs using Items.

--- grails-app/domain/de/dewarim/goblin/item/Item.groovy
+++ grails-app/domain/de/dewarim/goblin/item/Item.groovy
@@ -1,47 +1,44 @@
 package de.dewarim.goblin.item
 
-import de.dewarim.goblin.Creature
 import de.dewarim.goblin.ItemLocation
 import de.dewarim.goblin.pc.PlayerCharacter
 import de.dewarim.goblin.pc.crafting.ProductionResource
 
 class Item {
-	/**
-	 * attributes: passive effects like resist fire, see invisible.
-	 * features: active effects for which you must use an item, like heal self.
-	 */
-    static hasMany = [resources:ProductionResource]
-	 //	String name
-	Integer uses = 1
-	Boolean equipped = false
+    /**
+     * attributes: passive effects like resist fire, see invisible.
+     * features: active effects for which you must use an item, like heal self.
+     */
+    static hasMany = [resources: ProductionResource]
+    Integer uses = 1
+    Boolean equipped = false
     Integer amount = 1 // if the itemType is stackable, amount may be larger than 1
-	Long goldValue = 0
+    Long goldValue = 0
     ItemLocation location = ItemLocation.ON_PERSON
     ItemType type
-    Creature owner
+    PlayerCharacter owner
 
-    Item(){}
+    Item() {}
 
-    Item (ItemType itemType, PlayerCharacter pc){
+    Item(ItemType itemType, PlayerCharacter pc) {
         this.type = itemType
         this.owner = pc
-//		type.addToItems(this)
         initItem()
     }
 
-	void initItem(){
-        if(type.usable){
-			uses = type.uses
-		}
-		goldValue = type.baseValue
-	}
+    void initItem() {
+        if (type.usable) {
+            uses = type.uses
+        }
+        goldValue = type.baseValue
+    }
 
-    void initItem(Integer amount){
+    void initItem(Integer amount) {
         initItem()
-        if(type.stackable){
-			this.amount = amount
-		}
-	}
+        if (type.stackable) {
+            this.amount = amount
+        }
+    }
 
     boolean equals(o) {
         if (is(o)) return true
@@ -70,5 +67,21 @@ class Item {
         result = 31 * result + (type != null ? type.hashCode() : 0)
         result = 31 * result + (owner != null ? owner.hashCode() : 0)
         return result
+    }
+
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", uses=" + uses +
+                ", equipped=" + equipped +
+                ", amount=" + amount +
+                ", goldValue=" + goldValue +
+                ", location=" + location +
+                ", type=" + type.name +
+                ", typeId=" + type.id +
+                ", owner=" + owner.name +
+                '}';
     }
 }
