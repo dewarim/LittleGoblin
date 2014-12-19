ce6a606a0201117b311e269df8c5ca4168dbf177
Ingo Wiarda <ingo_wiarda@dewarim.de>
2014-12-19 20:18:03 +0100

    Add documentation

--- src/java/de/dewarim/goblin/ItemLocation.java
+++ src/java/de/dewarim/goblin/ItemLocation.java
@@ -1,10 +1,19 @@
 package de.dewarim.goblin;
 
 /**
- *
+ * Location of items owned by a player character.
  */
 public enum ItemLocation {
 
-    AT_HOME, ON_PERSON
+    /**
+     * Item is in the player character's base or home. 
+     * It should only be usable for crafting / trading.
+     */
+    AT_HOME, 
+    
+    /**
+     * Item is being carried around and may be used or equipped.
+     */ 
+    ON_PERSON
 
 }
