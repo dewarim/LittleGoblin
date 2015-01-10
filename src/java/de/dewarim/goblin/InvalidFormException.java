6feda44a19825554a853079f44b2957852f30ca7
Ingo Wiarda <ingo_wiarda@dewarim.de>
2015-01-10 20:38:52 +0100

    Add serial version id

--- src/java/de/dewarim/goblin/InvalidFormException.java
+++ src/java/de/dewarim/goblin/InvalidFormException.java
@@ -4,6 +4,8 @@ package de.dewarim.goblin;
  */
 public class InvalidFormException extends RuntimeException{
 
+    private static final long serialVersionUID = 2681378907930109715L;
+
     public InvalidFormException(String message, Throwable cause) {
         super(message, cause);
     }
